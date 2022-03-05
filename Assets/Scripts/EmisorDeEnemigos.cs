ce066cbf400836156f867528549a5cc7cb2c9741
Elián <elian.fabian.el.belaizi@gmail.com>
2022-03-05 13:31:52 +0100

    Emisor de enemigos añadido

---
+++ Assets/Scripts/EmisorDeEnemigos.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class EmisorDeEnemigos : MonoBehaviour
+{
+    [SerializeField] Enemigo enemigo;
+
+    private void Start()
+    {
+        StartCoroutine(Co_EmitirEnemigos());
+    }
+
+    System.Collections.IEnumerator Co_EmitirEnemigos()
+    {
+        while (true)
+        {
+            Instantiate(enemigo, transform.position, enemigo.transform.rotation);
+
+            var segundosAleatorios = Random.Range(2.0f, 5.0f);
+
+            yield return new WaitForSeconds(segundosAleatorios);
+        }
+    }
+}
--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -2,20 +2,32 @@ using UnityEngine;
 
 public class Enemigo : MonoBehaviour
 {
-    [SerializeField] Vector2 Velocidad = new Vector2(2, 3);
     [SerializeField] float Amplitud = 3;
 
     const string TAG_BALA_JUGADOR = "Bala Jugador";
 
+    Vector2 Velocidad;
     float Desfase;
+
+    const float limitIzquierdo = -14;
 
     private void Start()
     {
         Desfase = Random.Range(0.0f, 10.0f);
+
+        var x = Random.Range(1.0f, 2.0f);
+        var y = Random.Range(1.0f, 3.0f);
+
+        Velocidad = new Vector2(x, y);
     }
     void Update()
     {
         Mover();
+
+        if (transform.position.x < limitIzquierdo)
+        {
+            Destruir();
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
